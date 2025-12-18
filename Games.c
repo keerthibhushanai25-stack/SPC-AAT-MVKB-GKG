59a44422ed5e6421c759b36972372affd093c8d3
keerthibhushanai25-stack <keerthibhushan.ai25@bmsce.ac.in>
2025-12-18 13:53:17 +0530

    Add files via upload

--- Games.c
+++ Games.c
@@ -1,70 +1,87 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include<stdio.h>      // For input and output functions
+#include<stdlib.h>     // For rand(), srand(), exit()
+#include<time.h>       // For time() function
 
+/* ---------------- ROCK PAPER SCISSOR GAME ---------------- */
 int stonepc()
 {
-    int random = rand() % 4;
-    int input,i;
+    int random = rand() % 4;   // To store computer's choice
+    int input, i;
+
     printf("\n\n----WELCOME TO STONE PAPER SCISSOR GAME----\n\n");
-    for(i=0;i>=0;i++)
+
+    // Infinite loop until user exits
+    for(i = 0; i >= 0; i++)
     {
-        random = rand() % 4;
-        printf("\n\nEnter your input,\n1 for stone\n2 for paper\n3 for scissor\n0 to EXIT: ");
+        random = rand() % 4;   // Generate random number (0 to 3)
+
+        printf("\n\nEnter your input,\n");
+        printf("1 for stone\n2 for paper\n3 for scissor\n0 to EXIT: ");
         scanf(" %d", &input);
-            if(input == random)
-            {
-                printf("Computer's input: %d", random);
-                printf("\nDraw!!");
-            }
-            else if(input == 1 && random == 2)
-            {
-                printf("Computer's input: %d", random);
-                printf("\nWinner: Computer");
-            }
-            else if(input == 1 && random == 3)
-            {
-                printf("Computer's input: %d", random);
-                printf("\nWinner: You");
-            }
-            else if(input == 2 && random == 1)
-            {
-                printf("Computer's input: %d", random);
-                printf("\nWinner: You");
-            }
-            else if(input == 2 && random == 3)
-            {
-                printf("Computer's input: %d", random);
-                printf("\nWinner: Computer");
-            }
-            else if(input == 3 && random == 1)
-            {
-                printf("Computer's input: %d", random);
-                printf("\nWinner: Computer");
-            }
-            else if(input == 3 && random == 2)
-            {
-                printf("Computer's input: %d", random);
-                printf("\nWinner: You");
-            }
-            else if(input == 0)
-            {
-                break;
-            }
-            else
-            {
-                printf("\nInvalid input");
-            }
+
+        // If both inputs are same
+        if(input == random)
+        {
+            printf("Computer's input: %d", random);
+            printf("\nDraw!!");
+        }
+        // All winning and losing conditions
+        else if(input == 1 && random == 2)
+        {
+            printf("Computer's input: %d", random);
+            printf("\nWinner: Computer");
+        }
+        else if(input == 1 && random == 3)
+        {
+            printf("Computer's input: %d", random);
+            printf("\nWinner: You");
+        }
+        else if(input == 2 && random == 1)
+        {
+            printf("Computer's input: %d", random);
+            printf("\nWinner: You");
+        }
+        else if(input == 2 && random == 3)
+        {
+            printf("Computer's input: %d", random);
+            printf("\nWinner: Computer");
+        }
+        else if(input == 3 && random == 1)
+        {
+            printf("Computer's input: %d", random);
+            printf("\nWinner: Computer");
+        }
+        else if(input == 3 && random == 2)
+        {
+            printf("Computer's input: %d", random);
+            printf("\nWinner: You");
+        }
+        // Exit condition
+        else if(input == 0)
+        {
+            break;
+        }
+        // Invalid input
+        else
+        {
+            printf("\nInvalid input");
+        }
     }
+
     printf("Thanks for playing.....");
     return 0;
 }
 
-void ttt() {
-    char board[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+/* ---------------- TIC TAC TOE GAME ---------------- */
+void ttt()
+{
+    char board[9] = {' ',' ',' ',' ',' ',' ',' ',' ',' '}; // Game board
     int player = 1, move, i;
 
-    for (i = 0; i < 9; i++) {
+    // Maximum 9 moves in tic tac toe
+    for(i = 0; i < 9; i++)
+    {
+        // Display board
         printf("\n");
         printf(" %c | %c | %c\n", board[0], board[1], board[2]);
         printf("---+---+---\n");
@@ -76,24 +93,28 @@ void ttt() {
         printf("Enter 0 to EXIT: ");
         scanf("%d", &move);
 
-        // 🔴 EXIT condition
-        if (move == 0) {
+        // Exit the game
+        if(move == 0)
+        {
             printf("\nExiting Tic-Tac-Toe...\n");
             return;
         }
 
-        move--;  // convert to index
+        move--;  // Convert position to array index
 
-        if (move < 0 || move > 8 || board[move] != ' ') {
+        // Check for invalid move
+        if(move < 0 || move > 8 || board[move] != ' ')
+        {
             printf("Invalid move! Try again.\n");
-            i--;
+            i--;   // Reduce count for retry
             continue;
         }
 
+        // Assign symbol to board
         board[move] = (player == 1) ? 'X' : 'O';
 
-        // Check win
-        if (
+        // Check winning conditions
+        if(
             (board[0]==board[1] && board[1]==board[2] && board[0]!=' ') ||
             (board[3]==board[4] && board[4]==board[5] && board[3]!=' ') ||
             (board[6]==board[7] && board[7]==board[8] && board[6]!=' ') ||
@@ -102,52 +123,62 @@ void ttt() {
             (board[2]==board[5] && board[5]==board[8] && board[2]!=' ') ||
             (board[0]==board[4] && board[4]==board[8] && board[0]!=' ') ||
             (board[2]==board[4] && board[4]==board[6] && board[2]!=' ')
-           ) {
+          )
+        {
             printf("\nPlayer %d wins! 🎉\n", player);
             return;
         }
 
+        // Switch player
         player = (player == 1) ? 2 : 1;
     }
 
     printf("\nIt's a draw! 🤝\n");
 }
 
+/* ---------------- NUMBER GUESSING GAME ---------------- */
 int numguess()
 {
-    srand(time(0));
-    int r = rand() % 101;
+    srand(time(0));           // Seed random number
+    int r = rand() % 101;     // Random number between 0 and 100
     int g;
     int count = 0;
+
     printf("\n\n----WELCOME TO NUMBER GUESSING GAME----\n\n");
     printf("Enter -1 to exit\n");
     printf("Please enter your guess number(0-100): ");
     scanf("%d", &g);
+
     count++;
-    while(r!=g)
+
+    // Loop until correct guess
+    while(r != g)
     {
-    if(g == -1)
-    {
-        break;
+        if(g == -1)
+        {
+            break;
+        }
+        else if(r < g)
+        {
+            printf("\nMy number is less than your guess.");
+            printf("\nTry again!!\n");
+            count++;
+        }
+        else if(r > g)
+        {
+            printf("\nMy number is greater than your guess.");
+            printf("\nTry again!!\n");
+            count++;
+        }
+
+        printf("\nPlease enter your guess number: ");
+        scanf("%d", &g);
     }
-    else if(r<g)
+
+    // If user didn't exit
+    if(g != -1)
     {
-        printf("\nMy number is less than your guess.");
-        printf("\nTry again!!\n");
-        count++;
-    }
-    else if(r>g)
-    {
-        printf("\nMy number is greater than your guess.");
-        printf("\nTry again!!\n");
-        count++;
-    }
-    printf("\nPlease enter your guess number: ");
-    scanf("%d", &g);
-    }
-    if(g!=-1)
-    {
-    printf("\nYour guess is correct, you won.");
+        printf("\nYour guess is correct, you won.");
         printf("\nYou took %d attempts to guess the number.\n", count);
     }
 
@@ -155,10 +186,12 @@ int numguess()
     return 0;
 }
 
-int main() 
+/* ---------------- MAIN MENU ---------------- */
+int main()
 {
     char n;
-    label:
+
+label:
     printf("Enter the game you want to play:-\n");
     printf("\nPress 1 for number guess game.");
     printf("\nPress 2 for tic-tac-toe game.");
@@ -166,24 +199,30 @@ int main()
     printf("\nEnter 4 to EXIT!!");
     printf("\nEnter your input: ");
     scanf(" %c", &n);
+
+    // Menu selection
     while(n=='1' || n=='2' || n=='3' || n=='4')
     {
-    switch(n)
-    {
-        case '1':
+        switch(n)
+        {
+            case '1':
                 numguess();
                 break;
-        case '2':
+
+            case '2':
                 ttt();
                 break;
-        case '3':
+
+            case '3':
                 stonepc();
                 break;
-        case '4':
+
+            case '4':
                 exit(0);
+        }
+        break;
     }
-    break;
-    }
+
     printf("\nPlease enter a valid input\n\n");
     goto label;
 }
